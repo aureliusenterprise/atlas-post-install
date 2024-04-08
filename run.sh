d62c919332cefa40a2b75c782fe65822ca7e9876
matjgora <mateusz.gora@aureliusenterprise.com>
2024-04-08 13:49:19 +0200

    add an optional uploading of sample data to run.sh; update flink-ci submodule

--- run.sh
+++ run.sh
@@ -4,3 +4,11 @@ set -e
 
 python init_app_search_engines.py
 python init_atlas_types.py
+
+if [[ "$UPLOAD_DATA" == "true" ]]
+then
+    echo "Uploading sample data..."
+    ./upload_sample_data.sh
+else
+    echo "Skipping uploading sample data."
+fi
