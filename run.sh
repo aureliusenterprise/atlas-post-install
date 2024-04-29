27081e0308922216bd95747e32e77624bfce14c2
matjgora <mateusz.gora@aureliusenterprise.com>
2024-04-29 13:58:03 +0200

    add submitting flink jobs

--- run.sh
+++ run.sh
@@ -5,6 +5,11 @@ set -e
 python init_app_search_engines.py
 python init_atlas_types.py
 
+pushd /opt/flink/py_libs/flink_jobs/
+/opt/flink/bin/flink run -d --jobmanager "$JOBMANAGER_URL" -py synchronize_app_search.py
+/opt/flink/bin/flink run -d --jobmanager "$JOBMANAGER_URL" -py publish_state.py
+popd
+
 if [[ "$UPLOAD_DATA" == "true" ]]
 then
     echo "Uploading sample data..."
