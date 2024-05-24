3effd5c70ee4f8cfc677ef0c9c4656fa0a71e5cf
matjgora <mateusz.gora@aureliusenterprise.com>
2024-05-24 16:21:06 +0200

    add scripts for downloading data and updating it

--- init_app_search_engines.py
+++ init_app_search_engines.py
@@ -1,5 +1,7 @@
+import json
 import os
 import logging
+from pathlib import Path
 import requests
 from elastic_enterprise_search import AppSearch
 from elastic_enterprise_search.exceptions import BadRequestError
@@ -8,6 +10,7 @@ from requests.auth import HTTPBasicAuth
 
 from app_search_engine_setup import engines
 from index_template import publish_state_template
+from update_gov_index import put_all_documents
 
 
 NAMESPACE = os.getenv("NAMESPACE", "demo")
@@ -21,7 +24,15 @@ enterprise_search_url = os.getenv(
 )
 elastic_username = os.getenv("ELASTIC_USERNAME", "elastic")
 elastic_password = os.getenv("ELASTIC_PASSWORD", "elastic")
-elastic_certificate_path = os.getenv("ELASTIC_CERTIFICATE_PATH", None)
+elastic_certificate_path = os.getenv("ELASTIC_CERTIFICATE_PATH", "")
+
+UPLOAD_DATA = os.getenv("UPLOAD_DATA", False)
+
+
+ENGINES_TO_UPLOAD = {
+    "atlas-dev-quality": Path("data/atlas-dev-quality.json"),
+    "atlas-dev-gov-quality": Path("data/atlas-dev-gov-quality.json"),
+}
 
 
 def get_enterprise_api_private_key(
@@ -35,7 +46,7 @@ def get_enterprise_api_private_key(
     return key_info["key"]
 
 
-def put_index_template(elastic_client):
+def put_index_template(elastic_client: Elasticsearch) -> None:
     elastic_client.indices.put_index_template(
         name="publish-state-template",
         index_patterns=["publish_state"],
@@ -44,7 +55,7 @@ def put_index_template(elastic_client):
     )
 
 
-def create_engines(app_search_client):
+def create_engines(app_search_client: AppSearch) -> None:
     for engine in engines:
         try:
             app_search_client.create_engine(engine_name=engine["name"])
@@ -74,9 +85,18 @@ def create_engines(app_search_client):
             )
 
 
+def upload_indices(app_search_client: AppSearch) -> None:
+    for engine_name, data_path in ENGINES_TO_UPLOAD.items():
+        with open(data_path, "r") as json_file:
+            documents = json.load(json_file)
+        put_all_documents(app_search_client, engine_name, documents)
+
+
 def main():
     elastic_client = Elasticsearch(
-        hosts=[elastic_url], basic_auth=(elastic_username, elastic_password), ca_certs=elastic_certificate_path
+        hosts=[elastic_url],
+        basic_auth=(elastic_username, elastic_password),
+        ca_certs=elastic_certificate_path,
     )
 
     put_index_template(elastic_client)
@@ -88,6 +108,8 @@ def main():
     app_search_client = AppSearch(enterprise_search_url, bearer_auth=app_search_api_key)
 
     create_engines(app_search_client)
+    if UPLOAD_DATA == "true":
+        upload_indices(app_search_client)
 
 
 if __name__ == "__main__":
