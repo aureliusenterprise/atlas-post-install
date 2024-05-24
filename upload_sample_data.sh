d438b80b2c609ee537a02c888dcdc50db90b0db4
matjgora <mateusz.gora@aureliusenterprise.com>
2024-05-24 16:21:47 +0200

    change the way atlas data is uploaded

--- upload_sample_data.sh
+++ upload_sample_data.sh
@@ -2,8 +2,6 @@
 TOKEN=$(curl -d "client_id=$KEYCLOAK_CLIENT_ID" -d "username=$KEYCLOAK_USERNAME" -d "password=$KEYCLOAK_ATLAS_ADMIN_PASSWORD" -d 'grant_type=password' \
     "${KEYCLOAK_SERVER_URL}realms/${KEYCLOAK_REALM_NAME}/protocol/openid-connect/token" | jq .access_token)
 
-curl -g -v -X POST -H "Authorization: Bearer ${TOKEN:1:-1}" \
-                -H "Content-Type: multipart/form-data" \
-                -H "Cache-Control: no-cache" \
-                -F data=@data/sample_data.zip \
-                "${ATLAS_SERVER_URL}/admin/import"
+python export_atlas.py --token "$TOKEN" \
+--base-url "$ATLAS_SERVER_URL"\
+--output "data/sample_data.zip"
